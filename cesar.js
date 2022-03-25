971742e47986335b0a4e296a1cf098672866fd28
louis simeon <louis.simeon@gmx.fr>
2022-03-26 00:02:40 +0100

    cesar cipher is now working correctly

--- cesar.js
+++ cesar.js
@@ -1,4 +1,4 @@
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' '];
 var sentence = prompt('Enter the text you want to encrypt');
 var key = Number(prompt('Enter the key you want to use for encrypt your text'));
 var result = '';
@@ -7,10 +7,11 @@ if ((key <= 0) || (key >= 27) || (isNaN(key))) {
   document.write('Invalid key, please enter a number between 1 and 26');
 } else {
   for (var i = 0; i < sentence.length; i++) {
-    if (c === ' ') {
-      result += c;
+    var array1 = sentence.split('');
+    var c = array1[i];
+    if (alphabet.indexOf(c) === 26) {
+      result += ' ';
     } else {
-      var c = sentence[i];
       var id = alphabet.indexOf(c) + key;
       c = alphabet[id % 26];
       result += c;
